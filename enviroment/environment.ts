e9b9407fd43f834e487f925a25255d95b2429da5
christian.ingermann <christian.ingermann@dns.de>
2025-10-01 18:39:48 +0200

    env update

--- enviroment/environment.ts
+++ enviroment/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
-  apiUrl: "http://localhost:3000/api/",
+  apiUrl: "https://academics-backend-v8sx.onrender.com/api",
   leagueApiUrl: "https://127.0.0.1:2999/liveclientdata/activeplayer"
 }
